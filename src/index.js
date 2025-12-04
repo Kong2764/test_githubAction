fa63b5afe7aee3d17660f0ff25a21e4e755eb86d
jyg <01kong27@gmail.com>
2025-12-04 11:17:58 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// computes a factorial of a given positive integer
+function factorial(n) {
+
+    if( n < 0 ) {
+        throw new Error('n must be a positive integer');
+    }
+
+
+    if( n === 0 ) {
+        return 1;
+    }
+
+    return n * factorial(n - 1); //재귀적 함수(자기 자신 호출)
+}
+
+module.exports = factorial;
